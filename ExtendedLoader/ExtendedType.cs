99a2da86f485d82e6698478b69f23129d2dee77a
cyaminthe <99784235+Cyaminthe@users.noreply.github.com>
2022-07-03 22:16:33 +0400

    FaceOverride, fix MakeThumb, reparse skins
    Added a new enum FaceOverride, used as an attribute `face="Normal/Atk/Hit"` in head XML nodes. It forces the character to use the specified face with the motion (please note that side direction always forces Atk anyway). Also fixed assistant librarians using Normal faces for Fire, Aim, Special, and Slash-/Hit-/Penetrate2 motions - they will now use Atk just like patron librarians (unless overridden).
    Additional improvements to EffectPivot - it now can be used both in simple (x/y/rotation attributes) and complex (position/scale/euler nodes with x/y/z attributes) notations, with the latter taking precedence if present. Also, complex notation is now applicable to AdditionalPivot's and even Head!
    A possible fix for MakeThumbnail taking pictures of the UI - still in testing, so the actual working code is still commented out, but feel free to check it out.
    Additional improvement for parsing floats in skin files - now should be able to work with all cases. Additionally, as BaseMod takes priority order, skins are now reloaded regardless of their extended status, to make sure that potential float parsing errors are reparsed before other mods start loading.

--- ExtendedLoader/ExtendedType.cs
+++ ExtendedLoader/ExtendedType.cs
@@ -9,14 +9,20 @@ namespace ExtendedLoader
     public class ExtendedWorkshopSkinData : WorkshopSkinData
     {
         public List<BookSoundInfo> motionSoundList = new List<BookSoundInfo>();
-        public Dictionary<ActionDetail, Harmony_Patch.EffectPivot> specialMotionPivotDic = new Dictionary<ActionDetail, Harmony_Patch.EffectPivot>();
-        public Dictionary<string, Harmony_Patch.EffectPivot> atkEffectPivotDic = new Dictionary<string, Harmony_Patch.EffectPivot>();
+        public Dictionary<ActionDetail, EffectPivot> specialMotionPivotDic = new Dictionary<ActionDetail, EffectPivot>();
+        public Dictionary<string, EffectPivot> atkEffectPivotDic = new Dictionary<string, EffectPivot>();
     }
     public class ExtendedWorkshopAppearanceInfo : WorkshopAppearanceInfo
     {
         public List<BookSoundInfo> motionSoundList = new List<BookSoundInfo>();
-        public Dictionary<ActionDetail, Harmony_Patch.EffectPivot> specialMotionPivotDic = new Dictionary<ActionDetail, Harmony_Patch.EffectPivot>();
-        public Dictionary<string, Harmony_Patch.EffectPivot> atkEffectPivotDic = new Dictionary<string, Harmony_Patch.EffectPivot>();
+        public Dictionary<ActionDetail, EffectPivot> specialMotionPivotDic = new Dictionary<ActionDetail, EffectPivot>();
+        public Dictionary<string, EffectPivot> atkEffectPivotDic = new Dictionary<string, EffectPivot>();
+    }
+    public class EffectPivot
+    {
+        public Vector3 localPosition = Vector3.zero;
+        public Vector3 localScale = new Vector3(1, 1, 1);
+        public Vector3 localEulerAngles = Vector3.zero;
     }
     public class SkinPartRenderer : WorkshopSkinDataSetter.PartRenderer
     {
@@ -29,4 +35,15 @@ namespace ExtendedLoader
     {
 
     }
+    public class ExtendedCharacterMotion : CharacterMotion
+	{
+        public FaceOverride faceOverride = FaceOverride.None;
+	}
+    public enum FaceOverride
+	{
+        None = -1,
+        Normal = 0,
+        Hit = 6, 
+        Atk = 4
+	}
 }
